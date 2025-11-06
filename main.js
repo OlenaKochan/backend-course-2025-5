fe1018e982cdb88bd5cdd625feda082ab598b876
Olena Kochan bc2025-5 <olenakocan75@gmail.com>
2025-11-07 00:25:06 +0200

    Реалізовано метод PUT

--- main.js
+++ main.js
@@ -5,42 +5,77 @@ const path = require('path');
 const superagent = require('superagent');
 
 program
-  .option('-h, --host <host>','server host')
+  .option('-h, --host <host>', 'server host')
   .option('-p, --port <port>', 'server port')
-  .option('-c, --cache <dir>','path to cache directory')
+  .option('-c, --cache <dir>', 'path to cache directory')
 
 program.parse();
 const options = program.opts();
 
 if (!options.host) {
-    console.error('Please, specify host parameter');
-    process.exit(1);
+  console.error('Please, specify host parameter');
+  process.exit(1);
 }
 
 if (!options.port) {
-    console.error('Please, specify port parameter');
-    process.exit(1);
+  console.error('Please, specify port parameter');
+  process.exit(1);
 }
 
 if (!options.cache) {
-    console.error('Please, specify directory parameter');
-    process.exit(1);
+  console.error('Please, specify directory parameter');
+  process.exit(1);
 }
 
 const cacheDir = path.resolve(options.cache);
 
-if(!fs.existsSync(cacheDir)) {
-    fs.mkdirSync(cacheDir, { recursive: true });
-    console.log(`Created cache directory: ${cacheDir}`);
+if (!fs.existsSync(cacheDir)) {
+  fs.mkdirSync(cacheDir, { recursive: true });
+  console.log(`Created cache directory: ${cacheDir}`);
 } else {
-    console.log(`Cache directory exists: ${cacheDir}`);
+  console.log(`Cache directory exists: ${cacheDir}`);
 }
 
-const server = http.createServer( (req, res) => {
-   res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-   res.end(`Server is running. Cache directory: ${cacheDir}`);
+const server = http.createServer(async (req, res) => {
+  const method = req.method;
+  const code = req.url.slice(1);
+  const filepath = path.join(cacheDir, `${code}.jpg`);
+
+  console.log(`Request: ${method} ${req.url}`);
+
+  if (!/^\d+$/.test(code)) {
+    res.writeHead(405, { "Content-Type": "text/plain; charset=utf-8" });
+    return res.end("Method not allowed");
+  }
+
+  switch (method) {
+
+
+    case "PUT":
+      try {
+        const chunks = [];
+        for await (const chunk of req) {
+          chunks.push(chunk);
+        }
+        const img = Buffer.concat(chunks);
+        await fs.promises.writeFile(filepath, img);
+        res.writeHead(201, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end("Image saved to cache");
+      } catch (err) {
+        res.writeHead(500, { "Content-Type": "text/plain; charset=utf-8" });
+        res.end("Error writing file");
+      }
+      return; 
+
+
+
+    default:
+      res.writeHead(405, { "Content-Type": "text/plain; charset=utf-8" });
+      res.end("Method not allowed");
+      return;
+  }
 });
 
 server.listen(Number(options.port), options.host, () => {
-    console.log(`Server running at http://${options.host}:${options.port}/`);
+  console.log(`Server running at http://${options.host}:${options.port}/`);
 });
